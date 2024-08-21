af1213637f9edc97c22777e98876a9614cbf6408
Joy <joyhwangbvt2023@gmail.com>
2024-08-20 23:52:11 -0700

    Added a Reset State/Clear State to the Clear All and Filter buttons/text areas

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const itemForm = document.getElementById("item-form");
 const itemInput = document.getElementById("item-input");
 const itemList = document.getElementById("item-list");
 const clearButton = document.getElementById("clear");
+const itemFilter = document.getElementById("filter");
 
 // Create functions to be used
 function addItem (eventObj) {
@@ -28,14 +29,19 @@ function addItem (eventObj) {
 
         li.appendChild(button); // now add the button to the list item.
         
+        // Add li to the DOM
         itemList.appendChild(li);// add the list item, li, to the DOM
+
+        checkUI();
 
         itemInput.value = ""; // after all of that creation, clear the value (in the text node);
 }
 
 function createButton(classes) {
     const button = document.createElement("button"); // we want to append what is inside of the inside of the list item (by adding a button to the newly created list item), hence we use createElement("button"), where "button" is the inserted element.
+
     button.className = classes; // we want the button to have a class name. This is what we set earlier as the parameter for the input argument. Here, .className simply assigns the classes for said newly created button.
+
     const icon = createIcon("fa-solid fa-xmark"); // summon the createIcon() function.
     button.appendChild(icon); // append within the button itself to add icon into the button.
 
@@ -48,9 +54,14 @@ function createIcon(classes) {
     return icon; // return the icon!
 }
 
-function removeItem (e) {
-    if (e.target.parentElement.classList.contains("remove-item")){ // this will only remove the item that has a parent whose classes (i.e. its classList) contains the specific class of "remove-item", which is the button/icon in this case.
-    e.target.parentElement.parentElement.remove(); // traversing the DOM to first get to the parent (the button/icon) --> Then to the parent of the button (the list item)
+function removeItem(e) {
+    if (e.target.parentElement.classList.contains("remove-item")){ 
+        // this will only remove the item that has a parent whose classes (i.e. its classList) contains the specific class of "remove-item", which is the button/icon in this case.
+        if (window.confirm("Are you sure you want to Delete?")) {
+            e.target.parentElement.parentElement.remove(); // traversing the DOM to first get to the parent (the button/icon) --> Then to the parent of the button (the list item)
+
+            checkUI(); // we must use checkUI after removal is approved because we need to override and account for the changes in items.length
+        }
     }
 }
 
@@ -58,11 +69,28 @@ function clearItems(e) {
     // while the item list has a first child (i.e. the first list item)
     while (itemList.firstChild) { 
         itemList.removeChild(itemList.firstChild); // using remove Child on the ul, passing in the first child  
+
+        checkUI(); // we must use checkUI after removal is approved because we need to override and account for the changes in items.length
     }
 }
 
+// Creating a function to check UI: goal is to check the states of items
+function checkUI() {
+    const items = itemList.querySelectorAll("li"); // remember that when you use .querySelectorAll, you get the node list (similar to an array). 
+    // Remember that this is declared within the function so that every time the page is loaded up and the function is called, we intake the array length again as new.
+    if (items.length === 0) {
+        clearButton.style.display = "none";
+        itemFilter.style.display = "none";
+    }
+    else {
+        clearButton.style.display = "block";
+        itemFilter.style.display = "block";
+    }
+}
 
 // Set Up the necessary Event Listeners
 itemForm.addEventListener("submit", addItem); // calling function addItem
 itemList.addEventListener("click", removeItem); // calling function removeItem
 clearButton.addEventListener("click", clearItems); // to clear the items
+
+checkUI();
